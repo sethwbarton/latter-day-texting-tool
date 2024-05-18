53df4ac84bc639a9b96a974773b53f01c075e08e
sethbarton <seth.barton@integral.io>
2024-05-18 13:34:37 -0600

    Fix build

--- scripts/builds/arm64/build-arm64.py
+++ scripts/builds/arm64/build-arm64.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Latter-day Texting Tool - arm64',
-    app=['../../../src/main.py'],
+    app=['../../../main.py'],
     options={
         "py2app": {
             "arch": "arm64",
--- scripts/builds/x86/build-x86.py
+++ scripts/builds/x86/build-x86.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Latter-day Texting Tool - x86',
-    app=['../../../src/main.py'],
+    app=['../../../main.py'],
     options={
         "py2app": {
             "arch": "x86_64",
