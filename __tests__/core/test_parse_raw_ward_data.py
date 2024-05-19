7d3344c248edae100b43d383f4382f84f06074ee
sethbarton <seth.barton@integral.io>
2024-05-18 18:09:21 -0600

    Add some basic checks on the raw data.

--- __tests__/core/test_parse_raw_ward_data.py
+++ __tests__/core/test_parse_raw_ward_data.py
@@ -5,9 +5,9 @@ from src.core.parse_raw_ward_data import parse_raw_ward_data
 
 
 def get_example_member():
-    basepath = path.dirname(__file__)
-    filepath = path.abspath(path.join(basepath, "..", "example_data", "member.json"))
-    f = open(filepath)
+    base_path = path.dirname(__file__)
+    file_path = path.abspath(path.join(base_path, "..", "example_data", "member.json"))
+    f = open(file_path)
     data = json.load(f)
     f.close()
     return data
@@ -16,7 +16,7 @@ def get_example_member():
 def test_parse_raw_ward_data_includes_phone_number():
     test_data = get_example_member()
     result = parse_raw_ward_data(test_data)
-    assert result[0].phone_number == test_data[0]['phoneNumber']
+    assert result[0].phone_number == '1234567890'
 
 
 def test_parse_raw_ward_data_includes_first_name():
@@ -29,3 +29,58 @@ def test_parse_raw_ward_data_includes_last_name():
     data = get_example_member()
     result = parse_raw_ward_data(data)
     assert result[0].last_name == data[0]['nameFormats']['familyPreferredLocal']
+
+
+def test_parse_raw_ward_data_works_for_multiple_members():
+    member_one = get_example_member()[0]
+    member_two = get_example_member()[0]
+    member_two['nameFormats']['givenPreferredLocal'] = "John"
+    member_two['nameFormats']['familyPreferredLocal'] = "Doe"
+
+    result = parse_raw_ward_data([member_one, member_two])
+
+    assert result[0].phone_number == '1234567890'
+    assert result[0].first_name == member_one['nameFormats']['givenPreferredLocal']
+    assert result[0].last_name == member_one['nameFormats']['familyPreferredLocal']
+
+    assert result[1].phone_number == '1234567890'
+    assert result[1].first_name == member_two['nameFormats']['givenPreferredLocal']
+    assert result[1].last_name == member_two['nameFormats']['familyPreferredLocal']
+
+
+def test_parse_raw_ward_data_adds_note_for_elder_or_sister():
+    test_member = get_example_member()[0]
+
+    result = parse_raw_ward_data([test_member])
+
+    assert result[0].note == "Elders Quorum"
+
+
+def test_if_no_numbers_exist_skip_the_person():
+    member_with_no_number = get_example_member()[0]
+    member_with_no_number['phoneNumber'] = None
+
+    results = parse_raw_ward_data([member_with_no_number])
+
+    assert results == []
+
+
+def test_strips_all_non_nums_from_phone_number():
+    malformed_number = get_example_member()[0]
+    malformed_number['phoneNumber'] = '+1 444-444-4444 (his)'
+
+    results = parse_raw_ward_data([malformed_number])
+
+    assert results[0].phone_number == "14444444444"
+
+
+def test_ignores_members_with_missing_first_or_last_name():
+    missing_first_name = get_example_member()[0]
+    missing_first_name['nameFormats']['givenPreferredLocal'] = None
+
+    missing_last_name = get_example_member()[0]
+    missing_last_name['nameFormats']['familyPreferredLocal'] = None
+
+    results = parse_raw_ward_data([missing_first_name, missing_last_name])
+
+    assert results == []
--- src/core/parse_raw_ward_data.py
+++ src/core/parse_raw_ward_data.py
@@ -2,21 +2,39 @@ def parse_raw_ward_data(json_data):
     members = []
     for raw in json_data:
         members.append(parse_ward_member(raw))
-    return members
+    return [i for i in members if i is not None]
 
 
 def parse_ward_member(raw):
-    member = WardMember()
-    WardMember.phone_number = raw['phoneNumber']
-    WardMember.first_name = raw['nameFormats']['givenPreferredLocal']
-    WardMember.last_name = raw['nameFormats']['familyPreferredLocal']
+    phone_number = raw['phoneNumber']
+    first_name = raw['nameFormats']['givenPreferredLocal']
+    last_name = raw['nameFormats']['familyPreferredLocal']
+    if not phone_number or not first_name or not last_name:
+        return None
+    member = WardMember(phone_number=filter_numeric(phone_number),
+                        first_name=first_name,
+                        last_name=last_name,
+                        note=get_note(raw))
     return member
 
 
+def filter_numeric(input_string):
+    numeric_filtered = ''.join([char for char in input_string if char.isdigit()])
+    return numeric_filtered
+
+
+def get_note(raw):
+    if raw['sex'] == 'M':
+        return 'Elders Quorum'
+    return 'Relief Society'
+
+
 class WardMember:
-    phone_number = None
-    first_name = None
-    last_name = None
+    def __init__(self, phone_number=None, first_name=None, last_name=None, note=None):
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.first_name = first_name
+        self.note = note
 
     def to_csv_row(self):
         phone_number = self.phone_number
